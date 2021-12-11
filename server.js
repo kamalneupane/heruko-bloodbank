f85aedd97f4787776a0f91a1b20475ab1ce1fd94
Kamal Neupane <kamal12neupane@gmail.com>
2021-12-11 15:50:34 +0545

    package json file is modified

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ connectDatabase();
 
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server started on port ${process.env.PORT} in ${process.env.NODE_ENV} mode`)
+    console.log(`Server started on port ${process.env.PORT}`)
 })
 // handle unhandled promise rejection
 process.on('unhandledRejection', err => {
